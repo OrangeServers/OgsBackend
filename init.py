27b6c76884f12cf8fd21a9c424b4619a8d897bdd
xuwei777 <954157605@qq.com>
2021-07-13 11:26:16 +0800

    修复用户接口路径bug

--- init.py
+++ init.py
@@ -122,7 +122,7 @@ def orange_init_api():
     app.add_url_rule('/account/user/list_all', view_func=acc_user_list_all, methods=['POST', 'get'])
     app.add_url_rule('/account/user/add', view_func=acc_user_add, methods=['POST', 'get'])
     app.add_url_rule('/account/user/update', view_func=acc_user_update, methods=['POST', 'get'])
-    app.add_url_rule('/server/acc/user/del', view_func=acc_user_del, methods=['POST', 'get'])
+    app.add_url_rule('/account/user/del', view_func=acc_user_del, methods=['POST', 'get'])
 
     # 这里是系统用户接口
     app.add_url_rule('/server/sys/user/list', view_func=sys_user_list, methods=['POST', 'get'])
