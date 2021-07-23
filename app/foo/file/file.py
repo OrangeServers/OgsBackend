acd61187058ceba5d683ff3a86eb5ac42ebe3d8b
xuwei777 <954157605@qq.com>
2021-07-23 14:39:09 +0800

    优化数据返回格式

--- app/foo/file/file.py
+++ app/foo/file/file.py
@@ -6,15 +6,15 @@ class FileGet:
     def __init__(self):
         self.req_dir = request.values.get('req_dir', default='')
         self.def_dir_path = '/data/putfile/' + self.req_dir
-        self.dir_m = {}
-        self.file_m = {}
+        self.dir_list = []
+        self.file_list = []
 
     def get_file_list(self):
         dir_list = os.listdir(self.def_dir_path)
         for i in dir_list:
             file_type = os.path.isfile(self.def_dir_path + i)
             if file_type:
-                self.file_m[i] = {'name': i, 'type': 'file'}
+                self.file_list.append(i)
             else:
-                self.dir_m[i] = {'name': i, 'type': 'dir'}
-        return jsonify({'file': self.file_m, 'dir': self.dir_m})
+                self.dir_list.append(i)
+        return jsonify({'file': self.file_list, 'dir': self.dir_list})
