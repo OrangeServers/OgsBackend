6afbfea93ce0e4f63c03257698aed2fdfc7288d5
xuwei777 <954157605@qq.com>
2021-10-09 18:00:03 +0800

    新增继承key方法，待测试

--- app/tools/shellcmd.py
+++ app/tools/shellcmd.py
@@ -61,6 +61,40 @@ class RemoteConnection:
             self.put_file(i, to_path)
 
 
+# 新增继承方法，待测试
+class RemoteConnectionKey(RemoteConnection):
+    def __init__(self, host, port, username, pkey):
+        super(RemoteConnectionKey, self).__init__()
+        """
+        host-->远程连接的主机ip,str类型
+        port-->远程连接的主机ssh端口,int类型
+        username-->远程连接的主机用户名,str类型
+        password-->远程连接的主机用户密码,str类型
+        """
+        self.host = host
+        self.port = port
+        self.username = username
+        self.pkey = pkey
+
+    def ssh_cmd(self, command):
+        """
+        command-->需要执行的shell命令,str类型
+        """
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        key = paramiko.RSAKey.from_private_key_file(self.pkey)
+        ssh.connect(self.host, port=self.port, username=self.username, pkey=key, timeout=3)
+        stdin, stdout, stderr = ssh.exec_command(command)
+        # stdin_msg = stdin.read()
+        stdout_msg = stdout.read()
+        stderr_msg = stderr.read()
+        ssh.close()
+        if stdout_msg.decode() == '':
+            return stderr_msg.decode()
+        elif stderr_msg.decode() == '':
+            return stdout_msg.decode()
+
+
 if __name__ == '__main__':
     test_yw200 = RemoteConnection('10.0.1.200', 22, 'root', 'jlb123')
     test_yw200.ssh_cmd('hostname')
