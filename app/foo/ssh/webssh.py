b21e07ab2e51185695e780114c8c2d75ba2cbdcc
xuwei777 <954157605@qq.com>
2021-11-02 15:55:17 +0800

    新增根据系统用户登录ssh

--- app/foo/ssh/webssh.py
+++ app/foo/ssh/webssh.py
@@ -1,6 +1,6 @@
 from flask import request
 from Flask_App_Settings import app
-from app.sqldb.SqlAlchemyDB import t_host
+from app.sqldb.SqlAlchemyDB import t_host, t_sys_user
 from app.tools.basesec import BaseSec
 # pip install gevent-websocket导入IO多路复用模块
 from geventwebsocket.handler import WebSocketHandler  # 提供WS（websocket）协议处理
@@ -58,7 +58,21 @@ class ParSsh:
         return lt_msg
 
 
-client_list = []
+class ParSshKey(ParSsh):
+    def __init__(self, p_host, p_port, p_user, p_key):
+        self.host = p_host
+        self.port = p_port
+        self.user = p_user
+        self.key = paramiko.RSAKey.from_private_key_file(p_key)
+        self.trans = paramiko.Transport((self.host, self.port))  # 【坑1】 如果你使用 paramiko.SSHClient() cd后会回到连接的初始状态
+        self.trans.start_client()
+        self.trans.auth_publickey(username=self.user, key=self.key)
+        self.channel = self.trans.open_session()
+        self.channel.settimeout(10)
+        # 获取一个终端
+        self.channel.get_pty()
+        # 激活器
+        self.channel.invoke_shell()
 
 
 # @app.route('/websocket')
@@ -72,13 +86,17 @@ class OgsWebSocket:
             # 第一次接收数据转dict，新建ssh连接并核对用户名密码
             msg_one_cli = self.client_socket.receive()
             # json转dict
-            test1 = json.loads(msg_one_cli)
-            query_host_msg = t_host.query.filter_by(alias=test1['hostname']).first()
-            query_msg = query_host_msg.__dict__
-            # conn = ParSsh(hostname, port, username, password)
-            # 根据客户端发送的用户名密码连接
-            conn = ParSsh(query_msg['host_ip'], query_msg['host_port'], query_msg['host_user'],
-                          self.bse.base_de(query_msg['host_password']))
+            front_msg = json.loads(msg_one_cli)
+            query_host_msg = t_host.query.filter_by(alias=front_msg['hostname']).first()
+            # 查询系统用户信息
+            sys_user_info = t_sys_user.query.filter_by(alias=front_msg['username']).first()
+            # 查询主机信息和用户名密码，优先用key连接
+            if sys_user_info.host_key:
+                conn = ParSshKey(query_host_msg.host_ip, query_host_msg.host_port, sys_user_info.host_user,
+                                 sys_user_info.host_key)
+            else:
+                conn = ParSsh(query_host_msg.host_ip, query_host_msg.host_port, sys_user_info.host_user,
+                              self.bse.base_de(sys_user_info.host_password))
             # print(len(client_list), client_list)
             while 1:
                 msg_from_cli = self.client_socket.receive()
