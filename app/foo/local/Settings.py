81935f65a8fc2d29a7f3c32128a1a2b7a71507c5
xuwei777 <954157605@qq.com>
2022-02-21 16:16:15 +0800

    新增错误码，管理员权限才能修改设置

--- app/foo/local/Settings.py
+++ app/foo/local/Settings.py
@@ -17,17 +17,13 @@ class OgsSettings:
     @staticmethod
     def settings_open_info():
         default_msg = t_settings.query.filter_by(name='default').first()
-        return {'name': default_msg.name, 'login_time': default_msg.login_time, 'register_status': default_msg.register_status}
+        return {'name': default_msg.name, 'login_time': default_msg.login_time,
+                'register_status': default_msg.register_status}
 
     def settings_info(self):
-        query_msg = t_settings.query.filter_by(name=self.name).first()
-        if query_msg is None:
-            default_msg = t_settings.query.filter_by(name='default').first()
-            st_msg = self.lt.dict_reset_pop_auto(default_msg)
-            return jsonify(st_msg)
-        else:
-            st_msg = self.lt.dict_reset_pop_auto(query_msg)
-            return jsonify(st_msg)
+        default_msg = t_settings.query.filter_by(name='default').first()
+        st_msg = self.lt.dict_reset_pop_auto(default_msg)
+        return jsonify(st_msg)
 
     def settings_change(self):
         log_msg = 'req_body: [ name=%s ] /local/settings/update (fail)' % self.name
@@ -35,16 +31,19 @@ class OgsSettings:
             login_time = request.values.get('login_time')
             register_status = request.values.get('register_status')
             color_matching = request.values.get('color_matching')
-            query_msg = t_settings.query.filter_by(name=self.name).first()
-            if query_msg is None:
-                self.set_ins.ins_sql(self.name, login_time, register_status, color_matching)
-                return jsonify({'code': 0})
-            else:
-                t_settings.query.filter_by(name=self.name).update(
-                    {'name': self.name, 'login_time': login_time, 'register_status': register_status,
+            # query_msg = t_settings.query.filter_by(name=self.name).first()
+            # if query_msg is None:
+            #     self.set_ins.ins_sql(self.name, login_time, register_status, color_matching)
+            #     return jsonify({'code': 0})
+            # else:
+            if self.ords.conn.get(self.name + '_role') == 'admin':
+                t_settings.query.filter_by(name='default').update(
+                    {'login_time': login_time, 'register_status': register_status,
                      'color_matching': color_matching})
                 db.session.commit()
                 return jsonify({'code': 0})
+            else:
+                return jsonify({'code': 4})
         except IOError:
             Log.logger.info(log_msg + ' \"fail"')
             return jsonify({'code': 201})
