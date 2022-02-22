d172328d7519c7782a15690bfd7325683bbf6cda
xuwei777 <954157605@qq.com>
2022-02-22 16:54:22 +0800

    更新启动shell

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
-dir='/data/tmp/pycharm_ogsbackend/'
-/data/server/python371/bin/python3 ${dir}init.py
-# /data/server/python371/bin/python3 ${dir}webssh/run.py
+#!/bin/bash
+dir=$(pwd)
+/data/server/python371/bin/python3 ${dir}/init.py
+# /data/server/python371/bin/python3 ${dir}/webssh/run.py
