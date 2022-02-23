260cbdd7aab0581e13892b3a1f01cdca69e1e958
xuwei777 <954157605@qq.com>
2022-02-23 16:00:09 +0800

    修改shell脚本，删除无用包

--- start.sh
+++ start.sh
@@ -1,42 +1,36 @@
 #!/bin/bash
+# python环境变量路径，默认 python3 根据需求指定全路径
+python3_path='python3'
 dir=$(cd "$(dirname "$0")";pwd)
+
+function start_app() {
+  echo 'start orangeservers .......'
+  nohup $python3_path ${dir}/init.py > /data/logs/orangeservers.log 2>&1 &
+  [[ $? == 0 ]] && echo 'start orangeservers is ok' || echo 'start orangeservers error'; exit 4
+  echo 'start orangeservers .......'
+  nohup $python3_path ${dir}/ogsssh/run.py > /data/logs/orangessh.log 2>&1 &
+  [[ $? == 0 ]] && echo 'start orangessh is ok' || echo 'start orangessh error'; exit 4
+}
+
+function stop_app() {
+  echo 'stop orangeservers .......'
+  ps aux |grep "$python3_path ${dir}/init.py"|grep -v grep|awk '{print $2}'| xargs kill
+  [[ $? == 0 ]] && echo 'stop orangeservers is ok' || echo 'stop orangeservers error'; exit 4
+  echo 'stop orangessh .......'
+  ps aux |grep "$python3_path ${dir}/ogsssh/run.py"|grep -v grep|awk '{print $2}'| xargs kill
+  [[ $? == 0 ]] && echo 'stop orangessh is ok' || echo 'stop orangessh error'; exit 4
+}
+
 case $1 in
   'start')
-  echo 'start orangeservers .......'
-  nohup /data/server/python371/bin/python3 ${dir}/init.py > /data/logs/orangeservers.log 2>&1 &
-  [[ $? == 0 ]] && echo 'start orangeservers is ok' || exit 4
-  echo 'start orangeservers .......'
-  nohup /data/server/python371/bin/python3 ${dir}/ogsssh/run.py > /data/logs/orangessh.log 2>&1 &
-  [[ $? == 0 ]] && echo 'start orangessh is ok' || exit 4
+  start_app
   ;;
   'stop')
-  echo 'stop orangeservers .......'
-  ps aux |grep "/data/server/python371/bin/python3 ${dir}/init.py"|grep -v grep|awk '{print $2}'| xargs kill
-  [[ $? == 0 ]] && echo 'stop orangeservers is ok' || exit 4
-  echo 'stop orangessh .......'
-  ps aux |grep "/data/server/python371/bin/python3 ${dir}/ogsssh/run.py"|grep -v grep|awk '{print $2}'| xargs kill
-  [[ $? == 0 ]] && echo 'stop orangessh is ok' || exit 4
+  stop_app
   ;;
   'restart')
-    echo 'stop orangeservers .......'
-    ps aux |grep "/data/server/python371/bin/python3 ${dir}/init.py"|grep -v grep|awk '{print $2}'| xargs kill -9
-    if [ $? == 0 ]; then
-        echo 'start orangeservers .......'
-        nohup /data/server/python371/bin/python3 ${dir}/init.py > /data/logs/orangeservers.log 2>&1 &
-        [[ $? == 0 ]] && echo 'start orangeservers is ok' || exit 4
-    else
-      echo 'stop orangeservers is error!'
-      exit 4
-    fi
-    echo 'stop orangessh .......'
-    ps aux |grep "/data/server/python371/bin/python3 ${dir}/ogsssh/run.py"|grep -v grep|awk '{print $2}'| xargs kill
-    if [ $? == 0 ]; then
-        nohup /data/server/python371/bin/python3 ${dir}/init.py > /data/logs/orangeservers.log 2>&1 &
-        [[ $? == 0 ]] && echo 'start orangessh is ok' || exit 4
-    else
-      echo 'stop orangessh is error!'
-      exit 4
-    fi
+  stop_app
+  start_app
     ;;
     *)
     echo 'Please enter parameters start|stop|restart !'
