39999c12a1feac64c6fbe581d71c9e3d418d0910
xuwei777 <954157605@qq.com>
2022-02-23 10:05:56 +0800

    移除elastic apm链路追踪模块

--- init.py
+++ init.py
@@ -28,19 +28,19 @@ from skywalking import agent, config
 # 导入elastic apm链路追踪
 # from elasticapm.contrib.flask import ElasticAPM
 
-app.config['ELASTIC_APM'] = {
-    # Set required service name. Allowed characters:
-    # a-z, A-Z, 0-9, -, _, and space
-    'SERVICE_NAME': 'orangeserver',
+# app.config['ELASTIC_APM'] = {
+#     # Set required service name. Allowed characters:
+#     # a-z, A-Z, 0-9, -, _, and space
+#     'SERVICE_NAME': 'orangeserver',
+#
+#     # Use if APM Server requires a token
+#     'SECRET_TOKEN': '',
+#
+#     # Set custom APM Server URL (default: http://localhost:8200)
+#     'SERVER_URL': 'http://10.0.1.198:8200',
+# }
 
-    # Use if APM Server requires a token
-    'SECRET_TOKEN': '',
-
-    # Set custom APM Server URL (default: http://localhost:8200)
-    'SERVER_URL': 'http://10.0.1.198:8200',
-}
-
-apm = ElasticAPM(app)
+# apm = ElasticAPM(app)
 # 原有导入模块 --------------------------------------------------------------------------------------
 # from flask import render_template, make_response
 # from app.foo.user.decorator import wrapper
